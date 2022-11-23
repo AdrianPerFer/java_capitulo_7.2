3cfee0558ac5afd4bcb0e55f34670031152c05f6
AdrianPerFer <chopon007@gmail.com>
2022-11-23 20:03:11 +0100

    Ejercicios added

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -1,17 +1,13 @@
 /*
  * 
- * Escribe un programa que pida 20 números enteros. Estos números se deben
- * introducir en un array de 4 filas por 5 columnas. El programa mostrará las
- * sumas parciales de filas y columnas igual que si de una hoja de cálculo se
- * tratara. La suma total debe aparecer en la esquina inferior derecha.
+ * Modifica el programa anterior de tal forma que los números que se introducen
+ * en el array se generen de forma aleatoria (valores entre 100 y 999).
  * 
  * @author: Adrián Perogil Fernández
  * 
  */
-import java.util.Scanner;
-public class Ejercicio2 {
+public class Ejercicio3 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
         // Variables
         int [][] num = new int [4][5];
         int fila;
@@ -22,8 +18,8 @@ public class Ejercicio2 {
         // Input
         for (fila = 0; fila < 4; fila++) {
             for (columna = 0; columna < 5; columna++) {
-                System.out.print("Fila " + fila + ", columna " + columna + ": ");
-                num[fila][columna] = sc.nextInt();
+                int random = (int)(Math.random()*(1000-100)+100);
+                num[fila][columna] = random;
             }
         }
         System.out.println();
@@ -52,6 +48,5 @@ public class Ejercicio2 {
             System.out.printf("%7d   ", sumaColumna);
         }
         System.out.println("|  SUMA TOTAL = " + sumaTotal);
-        sc.close();
     }
 }
